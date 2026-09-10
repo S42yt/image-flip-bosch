cd5d658d1fe61d7fa0c988ea3a2bf6852bf24324
WuffWuffie <mikepolethedev@gmail.com>
2026-09-10 14:17:11 +0200

    update

--- Bot/Main.cs
+++ Bot/Main.cs
@@ -1,10 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using image_flip_bosch.Bot.ImgFlip;
 
 namespace image_flip_bosch.Bot
 {
-    internal class Main
+  internal class Program
+  {
+    public static async Task Main(string[] args)
     {
+      var builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddHttpClient<ImgFlipApi>(client =>
+      {
+        client.BaseAddress = new Uri("https://api.imgflip.com/");
+        client.Timeout = TimeSpan.FromSeconds(15);
+      });
+
+      var app = builder.Build();
+
+      var api = app.Services.GetRequiredService<ImgFlipApi>();
+      var res = await api.GetMemes();
+
+      foreach (var meme in res.ResponseImgFlipData.Memes)
+      {
+        System.Console.WriteLine($"{meme.Name}: {meme.Id}");
+      }
     }
+  }
 }
