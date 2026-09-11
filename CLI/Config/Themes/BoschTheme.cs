824e69b7d027f9fb4aec1c5e1e124c0ebc1d37ff
LearNivram <lwd1fe@bosch.com>
2026-09-11 08:35:32 +0200

    Test Themen Created

--- CLI/Config/Themes/BoschTheme.cs
+++ CLI/Config/Themes/BoschTheme.cs
@@ -1,3 +1,4 @@
+using SharpConsoleUI;
 using SharpConsoleUI.Themes;
 using System;
 using System.Collections.Generic;
@@ -7,5 +8,10 @@ namespace image_flip_bosch.CLI.Config.Themes
 {
   public class BoschTheme : ThemeBase
   {
+    public override string Name { get; set; } = "BoschTheme";
+    public override string Description { get; set; } = "Offiziele Themen von Bosch";
+    public override Color WindowBackgroundColor { get; set; } = Color.Wheat4;
+
+    public override Color WindowForegroundColor { get; set; } = Color.IndianRed;
   }
 }
