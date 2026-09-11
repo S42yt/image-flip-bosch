30c184fea7ac04cf132a3f9a545900165a328d3c
LearNivram <lwd1fe@bosch.com>
2026-09-11 08:26:22 +0200

    Config added and themes

---
+++ CLI/Config/Themes/BoschTheme.cs
@@ -0,0 +1,11 @@
+using SharpConsoleUI.Themes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace image_flip_bosch.CLI.Config.Themes
+{
+  public class BoschTheme : ThemeBase
+  {
+  }
+}
